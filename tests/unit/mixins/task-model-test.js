eaf748f9d50a85d3ef1b7f1288a42db51c32cd5a
allthesignals <wilburnforce@gmail.com>
2018-08-20 12:06:19 -0400

    Run codemods

--- tests/unit/mixins/task-model-test.js
+++ tests/unit/mixins/task-model-test.js
@@ -1,12 +1,12 @@
-import Ember from 'ember';
+import EmberObject from '@ember/object';
 import TaskModelMixin from 'ember-data-tasks/mixins/task-model';
 import { module, test } from 'qunit';
 
-module('Unit | Mixin | task model');
-
-// Replace this with your real tests.
-test('it works', function(assert) {
-  let TaskModelObject = Ember.Object.extend(TaskModelMixin);
-  let subject = TaskModelObject.create();
-  assert.ok(subject);
+module('Unit | Mixin | task model', function() {
+  // Replace this with your real tests.
+  test('it works', function(assert) {
+    let TaskModelObject = EmberObject.extend(TaskModelMixin);
+    let subject = TaskModelObject.create();
+    assert.ok(subject);
+  });
 });
--- tests/unit/mixins/task-store-test.js
+++ tests/unit/mixins/task-store-test.js
@@ -1,12 +1,12 @@
-import Ember from 'ember';
+import EmberObject from '@ember/object';
 import TaskStoreMixin from 'ember-data-tasks/mixins/task-store';
 import { module, test } from 'qunit';
 
-module('Unit | Mixin | task store');
-
-// Replace this with your real tests.
-test('it works', function(assert) {
-  let TaskStoreObject = Ember.Object.extend(TaskStoreMixin);
-  let subject = TaskStoreObject.create();
-  assert.ok(subject);
+module('Unit | Mixin | task store', function() {
+  // Replace this with your real tests.
+  test('it works', function(assert) {
+    let TaskStoreObject = EmberObject.extend(TaskStoreMixin);
+    let subject = TaskStoreObject.create();
+    assert.ok(subject);
+  });
 });
